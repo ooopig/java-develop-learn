03648e48a12c99128c4c37160ca3636ea6d46a49
201792080 <fanyuanxinV5@mail.dlut.edu.cn>
2020-10-20 23:59:08 +0800

    11

--- 专业课/test.java
+++ 专业课/test.java
@@ -1,8 +1,26 @@
 import java.lang.String;
 
-class test{
-    public static void main(String args[]){
-        ThreadLocal<Person> p = new ThreadLocal<>();
+ public class test{
+    static ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+    public static void main(String args[]) throws InterruptedException{
+        Thread t1 = new Thread(()->{
+            System.out.println(threadLocal.get());
+            threadLocal.set(0);
+            //threadLocal.set(5);
+            System.out.println(threadLocal.get());
+        });
+
+        Thread t2 = new Thread(()->{
+            System.out.println(threadLocal.get());
+            threadLocal.set(1);
+            System.out.println(threadLocal.get());
+        });
+
+        t1.start();
+        t1.join();
+        t2.start();
+
+        threadLocal.remove();
         
     }
 }
